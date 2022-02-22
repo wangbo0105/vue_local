572ac13d7b30125ff3e20cf5b595bdf1c4a60b46
wangbo0105 <820585630@qq.com>
2022-02-22 23:29:06 +0800

    init commit

---
+++ 20_github_搜索案例/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <search></search>
+    <List></List>
+  </div>
+</template>
+
+<script>
+import List from './components/List.vue'
+import Search from './components/Search.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    Search, List
+  }
+}
+</script>
